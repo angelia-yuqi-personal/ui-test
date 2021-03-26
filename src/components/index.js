d73d596ee16c30209ef7c26c59f869d7b7a70387
angelia <greenfish84@126.com>
2021-03-26 11:29:46 +0800

    feat: and simple import

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Alert } from "./Alert"
+export { default as Button } from "./Button"
+export { default as Input } from "./Input"
+export { default as Notification } from "./Notification"
+export { default as Select } from "./Select"
+export { default as Tag } from "./Tag"
+export { default as Toast } from "./Toast"
