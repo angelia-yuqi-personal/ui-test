022a924cf8262496e337d99cb98f22576bd8fa09
angelia <greenfish84@126.com>
2021-04-07 10:19:35 +0800

    feat: change the alert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -19,26 +19,22 @@ function Alert({
   }
   if (!open) return null
   return (
-    <div className="fixed w-full h-full top-0 left-0 flex justify-center z-10">
-      <div className="w-95">
-        <div
-          className={`flex relative items-start justify-start rounded-sm p-3 w-full bg-brand-blue-4 ${top} ${className}`}
-          {...props}
-        >
-          {icon && <img src={icon} alt="status" className="mr-2 w-4 h-4" />}
-          <div className={`text-sm text-brand-blue mr-${closable ? 6 : 0}`}>
-            {content}
-          </div>
-          {closable && (
-            <img
-              src={closeIcon || close}
-              alt="close"
-              className="absolute w-4 h-4 right-3"
-              onClick={e => onCloseClick(e)}
-            />
-          )}
-        </div>
+    <div
+      className={`flex relative items-start justify-start rounded-sm p-3 w-full bg-brand-blue-4 ${top} ${className}`}
+      {...props}
+    >
+      {icon && <img src={icon} alt="status" className="mr-2 w-4 h-4" />}
+      <div className={`text-sm text-brand-blue ${closable ? 'mr-6' : ''}`}>
+        {content}
       </div>
+      {closable && (
+        <img
+          src={closeIcon || close}
+          alt="close"
+          className="absolute w-4 h-4 right-3"
+          onClick={e => onCloseClick(e)}
+        />
+      )}
     </div>
   )
 }
